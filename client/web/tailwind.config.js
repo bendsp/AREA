68258ee99f759a82de32739810a0fee674416fe7
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-09-26 17:23:34 +0200

    feat(web): add tailwind

---
+++ client/web/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    // './components/**/*.{js,ts,jsx,tsx}',
+    // Add any additional paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
