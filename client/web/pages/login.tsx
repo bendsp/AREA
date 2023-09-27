0ba231e3bb75b64c15638d3d89949d1ba2a3437c
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-09-27 16:01:29 +0200

    feat(web): add register page & round corners

--- client/web/pages/login.tsx
+++ client/web/pages/login.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import Button from '@mui/material/Button';
+// import { TextField } from '@mui/material';
 import React from 'react';
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-[#050014]">
-      <div className="bg-[#282828] p-8 rounded shadow-lg">
+    <div className="flex flex-col items-center justify-center h-screen bg-[#050014]">
+      <div className="bg-[#282828] p-8 rounded-2xl shadow-lg">
         <h2 className="text-2xl text-white font-bold mb-4">Login</h2>
 
         <div className="mb-4">
@@ -13,7 +14,7 @@ const LoginPage: React.FC = () => {
             Username
           </label>
           <input
-            className="border rounded w-full py-2 px-3"
+            className="border rounded-xl w-full py-2 px-3"
             type="text"
             id="username"
             name="username"
@@ -26,7 +27,7 @@ const LoginPage: React.FC = () => {
             Password
           </label>
           <input
-            className="border rounded w-full py-2 px-3"
+            className="border rounded-xl w-full py-2 px-3"
             type="password"
             id="password"
             name="password"
@@ -34,18 +35,58 @@ const LoginPage: React.FC = () => {
           />
         </div>
 
-        <Button variant="contained" fullWidth className="bg-[#240090] hover:bg-[#3300CC]">
+        <Button variant="contained" fullWidth className="font-sans font-bold rounded-xl bg-[#240090] hover:bg-[#3300CC]">
           Login
         </Button>
 
         <Link href="/home">
-          <Button variant="outlined" fullWidth className="mt-5 border-white text-white hover:border-[#240090] hover:text-[#240090]">
+          <Button variant="outlined" fullWidth className="rounded-xl mt-5 border-white text-white hover:border-[#240090]">
             Go to Home
           </Button>
         </Link>
       </div>
+
+      <Link href="/register" className="mt-5">
+        <Button variant="contained" fullWidth className="font-sans font-bold rounded-xl bg-[#240090] hover:bg-[#3300CC]">
+          New user ?
+        </Button>
+      </Link>
     </div>
   );
 };
 
 export default LoginPage;
+
+// const LoginPage = () => {
+//   return (
+//     <div className="flex items-center justify-center h-screen bg-[#050014]">
+//       <div className="bg-[#282828] p-8 rounded shadow-lg">
+//         <h2 className="text-2xl text-white font-bold mb-6">Login</h2>
+//         <div className="mb-4">
+//           <TextField
+//             variant="outlined"
+//             fullWidth
+//             id="username"
+//             label="Username"
+//             InputProps={{ classes: { root: 'rounded-xl' } }}
+//           />
+//         </div>
+//         <div className="mb-6">
+//           <TextField
+//             variant="outlined"
+//             fullWidth
+//             type="password"
+//             id="password"
+//             label="Password"
+//             InputProps={{ classes: { root: 'rounded-xl' } }}
+//           />
+//         </div>
+//         <Button variant="contained" fullWidth className="bg-[#240090] hover:bg-[#3300CC]">
+//           Login
+//         </Button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LoginPage;
