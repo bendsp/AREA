04effdf0a6d7441fb08f8efd2e4d82659c68d914
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-10-02 15:38:48 +0200

    feat(web): protectedPages & NextRouter & interfaces

---
+++ client/web/components/HomeHeader.tsx
@@ -0,0 +1,9 @@
+const HomeHeader = () => {
+    return (
+        <div className="bg-white shadow-md p-5 mb-5 sticky top-0 z-20 rounded-2xl">
+            Hello Header
+        </div>
+    );
+}
+
+export default HomeHeader;
