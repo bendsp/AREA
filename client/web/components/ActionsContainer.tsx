f1d17220896e92df19dc346cbf7e7964eccc8a52
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-10-03 15:35:43 +0200

    feat(web): newAction button & newAction page

--- client/web/components/ActionsContainer.tsx
+++ client/web/components/ActionsContainer.tsx
@@ -1,8 +1,9 @@
 import ActionsCard from "./ActionsCard";
+import NewActionButton from "./NewActionButton";
 
 const ActionsContainer = () => {
     return (
-        <div className="bg-yellow-500 rounded-xl p-5 space-y-2">
+        <div className="bg-yellow-500 rounded-xl p-5 space-y-3">
             <h2 className="text-2xl mb-4 font-bold">Actions Container</h2>
                 <ActionsCard />
                 <ActionsCard />
@@ -10,6 +11,7 @@ const ActionsContainer = () => {
                 <ActionsCard />
                 <ActionsCard />
                 <ActionsCard />
+                <NewActionButton />
         </div>
     )
 }
