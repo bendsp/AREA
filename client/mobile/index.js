3b53b03792c0c395cbbe3279fe8414e0a305873e
Felix STAVONHAGEN <felix.stavonhagen@epitech.eu>
2023-10-04 11:06:03 +0200

    fix(mobile): fixed error with rootTag

--- client/mobile/index.js
+++ client/mobile/index.js
@@ -1,11 +1,5 @@
 import {AppRegistry} from 'react-native';
-import App from './App'; // Import your root component
+import App from './App';
 import {name as appName} from './app.json';
 
-// Register the root component for your app
 AppRegistry.registerComponent(appName, () => App);
-
-// Start the app by running the registered component
-AppRegistry.runApplication(appName, {
-  initialProps: {},
-});
