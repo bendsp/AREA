69c062faa944a2faae7d24e82539d70bfda8a011
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-10-04 16:03:54 +0200

    feat(web): fetch services from server & display in services card

--- client/web/components/ServicesCard.tsx
+++ client/web/components/ServicesCard.tsx
@@ -1,8 +1,32 @@
-const ServicesCard = () => {
+interface ServicesCardProps {
+    service: {
+        name: string;
+        actions: Array<string>;
+        reactions: Array<string>;
+    };
+}
+
+const ServicesCard = ({ service }: ServicesCardProps) => {
     return (
-        <div className="bg-green-700 rounded-2xl p-5">
-            <h3 className="text-xl mb-2 font-bold">Services Card</h3>
-            <p>Card content</p>
+        <div className="bg-green-700 rounded-2xl p-5 space-y-3">
+            <h3 className="text-xl font-bold">{service.name}</h3>
+            <div className="bg-white rounded-xl p-3">
+                {/* // TODO: change to description and add logo */}
+                <p className="font-bold">Actions:</p>
+                <ul>
+                    {service.actions.map((action) => {
+                        return <li key={action} className="italic">{action}</li>
+                    })}
+                </ul>
+            </div>
+            <div className="bg-white rounded-xl p-3">
+                <p className="font-bold">Reactions:</p>
+                <ul>
+                    {service.reactions.map((reaction) => {
+                        return <li key={reaction}>{reaction}</li>
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
--- client/web/components/ServicesContainer.tsx
+++ client/web/components/ServicesContainer.tsx
@@ -1,16 +1,23 @@
 import ServicesCard from "./ServicesCard";
 
-const ServicesContainer = () => {
+interface ServicesProps {
+    name: string;
+    actions: Array<string>;
+    reactions: Array<string>;
+}
+
+interface ServicesContainerProps {
+    services: Array<ServicesProps>;
+}
+
+const ServicesContainer = ({ services }: ServicesContainerProps) => {
     return (
         <div className="bg-green-500 rounded-xl p-5">
             <h2 className="text-2xl mb-4 font-bold">Services Container</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                <ServicesCard />
-                <ServicesCard />
-                <ServicesCard />
-                <ServicesCard />
-                <ServicesCard />
-                <ServicesCard />
+                {services.map((service) => {
+                    return <ServicesCard key={service.name} service={service} />
+                })}
             </div>
         </div>
     )
