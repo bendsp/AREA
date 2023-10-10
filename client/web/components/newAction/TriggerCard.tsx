cd3f1df4c3dd020fbcc449f152d8db3694886654
Paul THIEBAULT <paul.thiebault@epitech.eu>
2023-10-10 11:21:15 +0200

    feat(web): paramsValue in triggerCard

--- client/web/components/newAction/TriggerCard.tsx
+++ client/web/components/newAction/TriggerCard.tsx
@@ -8,10 +8,12 @@ interface TriggerCardProps {
 const TriggerCard = ({ services }: TriggerCardProps) => {
     const [selectedService, setSelectedService] = useState('');
     const [selectedAction, setSelectedAction] = useState('');
+    const [paramValues, setParamValues] = useState([])
 
     const handleApplyClick = () => {
         console.log(`Selected Service: ${selectedService}`);
         console.log(`Selected Action: ${selectedAction}`);
+        console.log('ParamValues:', paramValues);
     };
 
     useEffect(() => {
@@ -21,6 +23,9 @@ const TriggerCard = ({ services }: TriggerCardProps) => {
 
     return (
         <div className="flex flex-col bg-[#1e1e1e] text-[#121212] rounded-xl p-5 space-y-5">
+            <div className="bg-blue-500 w-fit rounded-xl px-2 text-lg font-bold text-[#1e1e1e]">
+                TRIGGER
+            </div>
             <div className="flex items-start space-x-4">
                 <div className="flex items-center space-x-4">
                     <label htmlFor="serviceDropdown" className="text-[#ffffff]">
@@ -39,10 +44,7 @@ const TriggerCard = ({ services }: TriggerCardProps) => {
                         ))}
                     </select>
                 </div>
-                <div className="ml-auto bg-[#ffffff] rounded-xl flex-1">
-                    {/* <label htmlFor="actionDropdown" className="text-[#121212]">
-                        Select an Action:
-                    </label> */}
+                <div className="ml-auto flex-1 space-y-3">
                     <select
                         id="actionDropdown"
                         className="border p-2 rounded w-full"
@@ -57,21 +59,23 @@ const TriggerCard = ({ services }: TriggerCardProps) => {
                                 </option>
                             ))}
                     </select>
-                    <div>
-                        Params
-                        {/* {services
+                    <div className="bg-white rounded-sm p-3">
+                        {services
                             .find((service) => service.name === selectedService)
                             ?.actions.find((action) => action.name === selectedAction)
                             ?.params.map((param, index) => (
-                                <div key={index}>
-                                    <label htmlFor={param.name}>{param.name}</label>
-                                    <input
-                                        type="text"
-                                        id={param.name}
-                                        className="border p-2 rounded w-full"
-                                    />
-                                </div>
-                            ))} */}
+                              <div key={index} className="flex flex-line space-x-5">
+                                <label htmlFor={param.name} className="pt-2">
+                                  {param.name}
+                                </label>
+                                <input
+                                  type="text"
+                                  id={param.name}
+                                  className="border p-2 rounded w-full"
+                                  onChange={(e) => setParamValues({ ...paramValues, [param.name]: e.target.value })}
+                                />
+                              </div>
+                            ))}
                     </div>
                 </div>
             </div>
